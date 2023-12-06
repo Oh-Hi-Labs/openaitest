e6659d2a50be2d240cd9f2b158582a84a79fa1fe
sfera studio <123723533+sferastudio@users.noreply.github.com>
2023-12-06 20:19:24 +0000

    Connected form on codepen to the OCR (hello-123) endpoing

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # openaitest
+
+## Description
+
+Prototype functions and api endpoints hosted on Supabase (Edge Functions)
--- supabase/functions/hello-123/index.ts
+++ supabase/functions/hello-123/index.ts
@@ -1,85 +1,96 @@
 const OPENAI_API_KEY = Deno.env.get('OPENAI_API_KEY');
+import { corsHeaders } from '../_shared/cors.ts'
 
 Deno.serve(async (req) => {
 
-  const { url } = await req.json()
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders })
+  }
+
+  const { imgUrl } = await req.json()
   const data = {
-    message: `Here's the url: ${url}`,
+    message: `Here's the url: ${imgUrl}`,
   }
   console.log(data)
 
-  // const url = 'https://api.openai.com/v1/chat/completions';
-  // const headers = {
-  //   'Content-Type': 'application/json',
-  //   'Authorization': 'Bearer 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',
-  //   'OpenAI-Organization': 'org-RgSCjOI2IIVQhBg1unz41kUu',
-  // };
+  const url = 'https://api.openai.com/v1/chat/completions';
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${OPENAI_API_KEY}`,
+    'OpenAI-Organization': 'org-RgSCjOI2IIVQhBg1unz41kUu',
+  };
 
-  // const body2 = {
-  //   model: "gpt-4-vision-preview",
-  //   messages: [
-  //     {
-  //       role: "user",
-  //       content: [
-  //         {
-  //           type: "text",
-  //           text: "Can you help me break down the menu items in the image into a JSON file that contains the following four keys - dish name, price, description, type or category. Only return the JSON in your response please"
-  //         },
-  //         {
-  //           type: "image_url",
-  //           image_url: {
-  //             url: "http://www.maithainy.com/wp-content/uploads/2018/01/menu-1.jpg"
-  //           }
-  //         }
-  //       ]
-  //     }
-  //   ],
-  //   "max_tokens": 1000
-  // };
+  const body = {
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "system", content: "You are a helpful assistant." }],
+  };
 
-  // try {
-  //   const response = await fetch(url, {
-  //     method: 'POST',
-  //     headers: headers,
-  //     body: JSON.stringify(body2)
-  //   });
+  const body2 = {
+    model: "gpt-4-vision-preview",
+    messages: [
+      {
+        role: "user",
+        content: [
+          {
+            type: "text",
+            text: "Can you help me break down the menu items in the image into a JSON file that contains the following four keys - dish name, price, description, type or category. Only return the JSON in your response please"
+          },
+          {
+            type: "image_url",
+            image_url: {
+              // url: "http://www.maithainy.com/wp-content/uploads/2018/01/menu-1.jpg"
+              url: imgUrl
+            }
+          }
+        ]
+      }
+    ],
+    "max_tokens": 1000
+  };
 
-  //   if (response.ok) {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(body2)
+    });
 
-  //     function parseSpecialJson(jsonString) {
-  //       // Remove the extra characters at the start and end of the string
-  //       const cleanedString = jsonString.replace(/```json\n|\n```/g, '');
+    if (response.ok) {
 
-  //       // Parse the cleaned string as JSON
-  //       try {
-  //         const parsedJson = JSON.parse(cleanedString);
-  //         console.log("Parsed JSON:", parsedJson);
-  //         return parsedJson;
-  //       } catch (e) {
-  //         console.error("Parsing error:", e);
-  //         return jsonString
-  //       }
-  //     }
+      function parseSpecialJson(jsonString) {
+        // Remove the extra characters at the start and end of the string
+        const cleanedString = jsonString.replace(/```json\n|\n```/g, '');
 
-  //     const data = await response.json();
-  //     console.log(data);
-  //     return new Response(
-  //       // JSON.stringify(data),
-  //       JSON.stringify(parseSpecialJson(data.choices[0].message.content)),
-  //       { headers: { "Content-Type": "application/json" } },
-  //     )
-  //   } else {
-  //     console.error('Response Error:', response.status);
-  //   }
-  // } catch (error) {
-  //   console.error('Fetch Error:', error);
-  // }
+        // Parse the cleaned string as JSON
+        try {
+          const parsedJson = JSON.parse(cleanedString);
+          console.log("Parsed JSON:", parsedJson);
+          return parsedJson;
+        } catch (e) {
+          console.error("Parsing error:", e);
+          return jsonString
+        }
+      }
 
-  let response = { status: "ok" }
+      const data = await response.json();
+      console.log(data);
+      return new Response(
+        // JSON.stringify(data),
+        JSON.stringify(parseSpecialJson(data.choices[0].message.content)),
+        { headers: { ...corsHeaders, "Content-Type": "application/json" } },
+      )
+    } else {
+      console.error('Response Error:', response.status);
+    }
+  } catch (error) {
+    console.error('Fetch Error:', error);
+  }
 
-  return new Response(JSON.stringify(response), {
-    headers: { "Content-Type": "application/json" },
-    status: 200,
-  })
+  // let response = { status: "ok" }
+
+  // return new Response(JSON.stringify(response), {
+  //   headers: { ...corsHeaders, "Content-Type": "application/json" },
+  //   status: 200,
+  // })
 
 })
